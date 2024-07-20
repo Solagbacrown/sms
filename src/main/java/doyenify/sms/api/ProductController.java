460578c66e46a975821bf22f4bc8a60eb2cbeee9
Sola <solagbadeenitilo@gmail.com>
2024-07-20 14:23:20 +0300

    remove space

--- src/main/java/doyenify/sms/api/ProductController.java
+++ src/main/java/doyenify/sms/api/ProductController.java
@@ -1,7 +1,6 @@
 package doyenify.sms.api;
 
 import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import doyenify.sms.entities.Product;
 import doyenify.sms.service.ProductService;
 
